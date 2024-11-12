8c00ca1a5364b111212b51d170bd245d608f2966
siddhi2218 <siddhi2218@mac.lan>
2024-11-11 23:19:35 -0600

    Stubbed necessary methods to fix any compiler errors.

--- src/edu/uwm/cs351/WordMultiset.java
+++ src/edu/uwm/cs351/WordMultiset.java
@@ -1,9 +1,11 @@
 package edu.uwm.cs351;
 import java.util.AbstractMap;
 import java.util.AbstractSet;
+import java.util.Collection;
 import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.NoSuchElementException;
 import java.util.Objects;
 import java.util.Set;
@@ -110,7 +112,6 @@ public class WordMultiset // extends something
 		assert wellFormed() : "invariant false at end of constructor";
 	}
 	
-	@Override // required
 	public int size() {
 		assert wellFormed() : "invariant false at start of size()";
 		return numEntries;
@@ -179,13 +180,91 @@ public class WordMultiset // extends something
 
 	private final EntrySet entrySet = new EntrySet();
 	
-	@Override // required
 	public Set<Map.Entry<String,Integer>> entrySet() {
 		return entrySet;
 	}
 	
 	private class EntrySet // extends something
+ implements Set<Entry<String, Integer>>
 	{
+
+		@Override
+		public int size() {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public boolean isEmpty() {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public boolean contains(Object o) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public Iterator<Entry<String, Integer>> iterator() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public Object[] toArray() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public <T> T[] toArray(T[] a) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public boolean add(Entry<String, Integer> e) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public boolean remove(Object o) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public boolean containsAll(Collection<?> c) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public boolean addAll(Collection<? extends Entry<String, Integer>> c) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public boolean retainAll(Collection<?> c) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public boolean removeAll(Collection<?> c) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public void clear() {
+			// TODO Auto-generated method stub
+			
+		}
 	}
 	
 	// TODO: Implement iterator class
